ab86406c320f33db5304765f16f7ee13008e950f
dugasalg <73446031+dugasalg@users.noreply.github.com>
2023-11-14 19:39:23 -0600

    Commit inicial

---
+++ src/AppLogin.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+    <b-container>
+      <b-row>
+        <b-col></b-col>
+        <b-col cols="6">
+          <b-card title="Login">
+            <b-form @submit="login">
+              <b-form-group
+                  label="Username"
+              >
+                <b-form-input
+                    v-model="username"
+                    placeholder="Enter your username"
+                    required
+                ></b-form-input>
+              </b-form-group>
+              <br>
+              <b-form-group
+                  label="Password"
+              >
+                <b-form-input
+                    v-model="pass"
+                    type="password"
+                    placeholder="Enter your password"
+                    required
+                ></b-form-input>
+              </b-form-group>
+              <br>
+              <b-button type="submit" variant="primary">Login</b-button>
+            </b-form>
+          </b-card>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data() {
+    return {
+      username:"",
+      pass:""
+    }
+  },
+  methods: {
+    async login(event){
+      event.preventDefault()
+      const requestBody = {
+      usrn: this.username,
+      password: this.pass
+      }
+
+      try {
+        const response = await axios.post('https://distinct-fish-polo-shirt.cyclic.app/users/iniciar-sesion', requestBody)
+        console.log(response)
+      } catch (error) {
+        console.error(error)
+      }
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/AppRegister.vue
@@ -0,0 +1,100 @@
+<template>
+  <div id="app">
+    <b-container>
+      <b-row>
+        <b-col></b-col>
+        <b-col cols="6">
+          <b-card title="Register">
+            <b-form @submit.prevent="register">
+              <b-form-group label="First Name">
+                <b-form-input
+                    v-model="firstName"
+                    placeholder="Enter your first name"
+                    required
+                ></b-form-input>
+              </b-form-group>
+
+              <b-form-group label="Last Name">
+                <b-form-input
+                    v-model="lastName"
+                    placeholder="Enter your last name"
+                    required
+                ></b-form-input>
+              </b-form-group>
+
+              <b-form-group label="Email">
+                <b-form-input
+                    v-model="email"
+                    type="email"
+                    placeholder="Enter your email"
+                    required
+                ></b-form-input>
+              </b-form-group>
+
+              <b-form-group label="Username">
+                <b-form-input
+                    v-model="username"
+                    placeholder="Enter your username"
+                    required
+                ></b-form-input>
+              </b-form-group>
+
+              <b-form-group label="Password">
+                <b-form-input
+                    v-model="password"
+                    type="password"
+                    placeholder="Enter your password"
+                    required
+                ></b-form-input>
+              </b-form-group>
+
+              <b-button type="submit" variant="primary">Register</b-button>
+            </b-form>
+          </b-card>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Register-form',
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      email: "",
+      username: "",
+      password: ""
+    }
+  },
+  methods: {
+    async register() {
+      const requestBody = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        username: this.username,
+        password: this.password
+      }
+
+      try {
+        const response = await axios.post('https://distinct-fish-polo-shirt.cyclic.app/users/registrar', requestBody)
+        console.log(response)
+        // Handle post registration logic (like redirecting to login page)
+      } catch (error) {
+        console.error(error)
+        // Handle errors (like displaying error message)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+/* You can use the same styles as your login form */
+</style>
